a5dfcf9a50180fdeee79ac56a82b6d058a9e7c8c
Vamsi Ravi <vamsi963601@gmail.com>
2020-08-04 10:22:05 +0530

    initial commit

---
+++ location.py
@@ -0,0 +1,19 @@
+import requests
+
+
+headers = {
+    'Accept': 'application/json',
+    'user-key': '64d77bbb8da06cbddfa478d08ef80aa0',
+    }
+
+# params = (
+#     ('query', 'Visakhapatnam'),
+# )
+url = 'https://developers.zomato.com/api/v2.1/locations?query=Visakhapatnam'
+response = requests.get(url, headers=headers)
+result = response.json()
+
+for res1 in result['location_suggestions']:
+    print(res1['latitude'])
+    print(res1['longitude'])
+
---
+++ search.py
@@ -0,0 +1,6 @@
+import location
+
+
+for res1 in location.result['location_suggestions']:
+    lat = res1['latitude']
+    long = res1['longitude']
